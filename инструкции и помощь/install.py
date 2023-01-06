5b0b14ddff908c34dc94372daaf4abc59fd4bf33
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-01-06 19:29:34 +0300

    Add files via upload

---
+++ инструкции и помощь/install.py
@@ -0,0 +1,13 @@
+import os
+
+os.system('pip install pyautogui')
+os.system('pip install datetime')
+os.system('pip install shutil')
+os.system('pip install pyTelegramBotAPI')
+os.system('pip install keyboard')
+os.system('pip install ctypes') 
+os.system('pip install winreg')
+os.system('pip install subprocess')
+os.system('pip install win32api')
+os.system('pip install pyinstaller')
+os.system('pip install configparser')
